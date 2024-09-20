0419a152157c99c905e657bd9bc9777d2e57ca5e
Samadsust71 <samadsust71@gmail.com>
2024-09-20 13:06:54 +0600

    made some correction

--- js/login.js
+++ js/login.js
@@ -1,8 +1,8 @@
 document.getElementById('login-btn').addEventListener('click',function(e){
     e.preventDefault();
     const numberValue = document.getElementById('number-box').value;
-    const pinValue = getInputValue('pin-box');
-    if (numberValue.length === 11 && pinValue === 1234) {
+    const pinValue = document.getElementById('pin-box').value;
+    if (numberValue.length === 11 && pinValue === '1234') {
         window.location.href = 'home.html'
     }
     else{
